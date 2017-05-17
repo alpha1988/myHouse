baaeb990b8a1c9e5eabf088d4bed5fd1b7b68c8c
Artem Salamakha <a_salamakha@mifprojects.com>
2017-05-17 15:13:32 +0300

    refactoring

---
+++ src/app/building/building.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BuildingComponent } from './building.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BuildingComponent]
+})
+export class BuildingModule { }
